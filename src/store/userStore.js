82ed9e1a57dbcf3b54bf928e0f95a3fa5ac95295
Sardorbek-Web <berkinovsardorbek2@gmail.com>
2026-04-05 09:26:54 +0500

    fix netlify functions folder

--- src/store/userStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useUserStore = create((set) => ({
-  telegramUser: null,
-  profile: null,
-
-  setTelegramUser: (telegramUser) => set({ telegramUser }),
-  setProfile: (profile) => set({ profile }),
-}));
